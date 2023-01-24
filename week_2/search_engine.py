bb19746617d341dd7d29b5f79a4bfb8eef2285cf
gruksd <sofia.gruk@helsinki.fi>
2023-01-24 17:34:02 +0200

    Update search_engine.py

--- week_2/search_engine.py
+++ week_2/search_engine.py
@@ -146,19 +146,83 @@ def code_from_jupyter_notebook():
 
 
 def main():
+    import re
+    from sklearn.feature_extraction.text import CountVectorizer
 
     
 
-    document = open("articles.txt", "r", encoding="utf-8")
-    document.read()
+    #document = open("articles.txt", "r", encoding="utf-8")
+    #document.read()
+    #article_title_list = re.findall(r"<article name=\"(\w+\S?
 
+
+    documents = ["This is a silly example",
+                "A better example",
+                "Nothing to see here",
+                "This is a great and long example"]
+
+    cv = CountVectorizer(lowercase=True, binary=True) 
+    sparse_matrix = cv.fit_transform(documents)
+
+    dense_matrix = sparse_matrix.todense()
+
+    td_matrix = dense_matrix.T
+    
+    terms = cv.get_feature_names_out()
+    t2i = cv.vocabulary_
+
+    d = {"AND": "&", "OR": "|",
+        "NOT": "1 -",
+        "(": "(", ")": ")"}          # operator replacements
+
+    def rewrite_token(t):
+        return d.get(t, 'td_matrix[t2i["{:s}"]]'.format(t)) # Can you figure out what happens here?
+
+    def rewrite_query(query): # rewrite every token in the query
+        return " ".join(rewrite_token(t) for t in query.split())
+
+    #def test_query(query):
+        #print("Query: '" + query + "'")
+        #print("Rewritten:", rewrite_query(query))
+        #print("Matching:", eval(rewrite_query(query))) # Eval runs the string as a Python command
+        #print()
+
+    #test_query("example AND NOT nothing")
+    #test_query("NOT example OR great")
+    #test_query("( NOT example OR great ) AND nothing") # AND, OR, NOT can be written either in ALLCAPS
+    #test_query("( not example or great ) and nothing") # ... or all small letters
+    #test_query("not example and not nothing")
+
+   # sparse_td_matrix = sparse_matrix.T.tocsr()
+
+    #def rewrite_token(t):
+     #   return d.get(t, 'sparse_td_matrix[t2i["{:s}"]].todense()'.format(t)) # Make retrieved rows dense
+
+    
 
     query ="*"
     while query != "":
         query = input("Type a query: ")
+        hits_matrix = eval(rewrite_query(query))
+        print("Matching documents as vector (it is actually a matrix with one single row):", hits_matrix)
+        print("The coordinates of the non-zero elements:", hits_matrix.nonzero())    
+        hits_list = list(hits_matrix.nonzero()[1])
+        print(hits_list)
+        
+        for doc_idx in hits_list:
+            docs_list = re.findall(r".{7,10}\b", documents[doc_idx])
+            docs_part = docs_list[0]
+            docs = "".join(docs_part)
+            print("Matching doc:", docs)
+
+        for i, doc_idx in enumerate(hits_list):
+            print("Matching doc #{:d}: {:s}".format(i, documents[doc_idx]))
+        
         if query == "":
             print("Goodbye!")
             break
+        
+    
         
     
 main()
