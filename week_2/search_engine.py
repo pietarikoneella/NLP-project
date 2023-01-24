b569f936ea97a03776c4fb2a6d78c57f05b1ae7b
gruksd <sofia.gruk@helsinki.fi>
2023-01-24 13:26:29 +0200

    Update search_engine.py

--- week_2/search_engine.py
+++ week_2/search_engine.py
@@ -1,9 +1,15 @@
 def main():
+
+    document = open("articles.txt", "r", encoding="utf-8")
+    document.read()
+
+
     query ="*"
     while query != "":
         query = input("Type a query: ")
         if query == "":
             print("Goodbye!")
             break
+        
     
 main()
