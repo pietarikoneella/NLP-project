29d376f5572c97d38175944263ed8df5561407cf
gruksd <sofia.gruk@helsinki.fi>
2023-02-07 13:13:48 +0200

    Update flask_search.py

--- week_4/flask_search.py
+++ week_4/flask_search.py
@@ -1,2 +1,4 @@
-Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+from flask import Flask, render_template, request
+
+#Initialize Flask instance
+app = Flask(__name__)
