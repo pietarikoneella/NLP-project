23bfca94f0c9631c6403fd0d5b91dfa14f9df7cc
gruksd <sofia.gruk@helsinki.fi>
2023-02-10 17:49:13 +0200

    Update flask_search.py

--- week_4/flask_search.py
+++ week_4/flask_search.py
@@ -15,28 +15,36 @@ if len(documents) == 100:
 # The rest of the code is copied from the example
 
 @app.route('/')
-def hello_world():
-   return "Hello, World!"
+def index():
+   return render_template("welcome_page.html")
 
+@app.route('/options')
+def optionts():
+    return render_template("options.html")
 #Function search() is associated with the address base URL + "/search"
-@app.route('/search')
-def search():
+
+@app.route('/boolean')
+def search_b():
 
     #Get query from URL variable
-    query = request.args.get('query')
+    #query = request.args.get('query')
 
     #Initialize list of matches
-    matches = []
+    #matches = []
 
     #If query exists (i.e. is not None)
-    if query:
+    #if query:
         #Look at each entry in the example data
-        for entry in example_data:
+        #for entry in example_data:
             #If an entry name contains the query, add the entry to matches
-            if query.lower() in entry['name'].lower():
-                matches.append(entry)
+            #if query.lower() in entry['name'].lower():
+                #matches.append(entry)
 
     #Render index.html with matches variable
-    return render_template('index.html', matches=matches)
+    return render_template('boolean.html') #matches=matches)
+
+@app.route('/td_idf')
+def search_t():
+    return render_template('td_idf.html') #matches=matches)
 
 
