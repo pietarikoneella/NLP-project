7adc661f5fbc51fe9c03637986722f3d59ed5136
Maria M <46593191+MariaKatariina@users.noreply.github.com>
2023-02-23 13:58:47 +0200

    continued working toward a dynamic movie page

--- final_project/__init__.py
+++ final_project/__init__.py
@@ -1,20 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 titles = ["Movie 1", "Movie 2", "Movie 3", "Movie 4"]
 synopses = ["Synopsis 1", "Synopsis 2", "Synopsis 3", "Synopsis 4"]
-data = zip(titles, synopses)
+themes = [["Theme 1", "theme 2", "theme 3"], ["Theme 2", "Theme 4", "Theme 5"], ["Theme 2", "Theme 3", "Theme 4", "Theme 5"], ["Theme 3", "Theme 4", "Theme 5", "Theme 6", ] ]
+data = zip(titles, synopses, themes)
+query = ""
 
 @app.route('/')
 def index():
     print("Hello, world")
-#    return render_template('index.html', data=data)
+    return render_template('index.html')
+
 
 @app.route('/index')
 def search():
 
     query = request.args.get('query')
+
     
     if query:
         print('The query is "' + query +'".')
