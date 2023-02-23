30557cac44caa284165c5d5ff59e3ba28f9fdfa3
Maria M <46593191+MariaKatariina@users.noreply.github.com>
2023-02-23 12:45:50 +0200

    working on dynamic html page

--- final_project/__init__.py
+++ final_project/__init__.py
@@ -2,29 +2,36 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+titles = ["Movie 1", "Movie 2", "Movie 3", "Movie 4"]
+synopses = ["Synopsis 1", "Synopsis 2", "Synopsis 3", "Synopsis 4"]
+data = zip(titles, synopses)
+
 @app.route('/')
+def index():
+    print("Hello, world")
+#    return render_template('index.html', data=data)
 
-def first():
-    return "Movie search" 
-
-@app.route('/search')
-
+@app.route('/index')
 def search():
 
-    #query = request.args.get('query')
-    #if query:
-    title = ["Great movie"]
-    synopsis = "Exciting plot, world-class actors, big themes, beautifully excecuted."
+    query = request.args.get('query')
+    
+    if query:
+        print('The query is "' + query +'".')
 
+    return render_template('index.html', data=data, query=query)
 
-    return render_template('search.html', title=title, synopsis=synopsis)
+@app.route('/movie/<title>')
 
-#@app.route('/movie')
+def show_movie(title):
+    print("Showing movie")
+
+    #title = request.args.get('query')
+
+    
+    return render_template('movie.html', title=title)
 
 #def show_movie(title, synopsis):
 
 #    return render_template('movie.html', title=title, synopsis=synopsis)
 
-
-
-    
