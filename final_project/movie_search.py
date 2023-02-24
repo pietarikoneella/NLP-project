d72620bc63c803def7f154a72e9e121263deac6d
Maria M <46593191+MariaKatariina@users.noreply.github.com>
2023-02-24 07:13:59 +0200

    added the program file where we can put all our functions so that __init__.py does not get too long to handle

---
+++ final_project/movie_search.py
@@ -0,0 +1,11 @@
+import nltk
+from nltk.stem import PorterStemmer
+from nltk import word_tokenize
+#nltk.download('punkt')
+import pke
+import numpy
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import matplotlib.pyplot as plt
+
