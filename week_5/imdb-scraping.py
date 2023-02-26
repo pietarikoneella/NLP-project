31a40c609faea00e098a07900342935f4011daab
pietarikoneella <pietari.padilla@gmail.com>
2023-02-26 14:55:59 +0200

    extract synopsis from plot page

--- week_5/imdb-scraping.py
+++ week_5/imdb-scraping.py
@@ -33,7 +33,9 @@ try:
     #req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     #webpage = urlopen(req).read()
     #print("Beginning of webpage:", webpage[:1000])
-    
+
+    """
+    #Extracting stuff from a movie's main page
     for url in urls:
         req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
         webpage = urlopen(req).read()
@@ -42,13 +44,38 @@ try:
 
         soup = BeautifulSoup(webpage, "html.parser")
 
-        #storyline = soup.find("div", class_="ipc-html-content-inner-div")
-        #synopsis = soup.find("a", class_="ipc-link ipc-link--base ipc-link--inline")
+        storyline = soup.find("div", class_="ipc-html-content-inner-div")
+        synopsis = soup.find("a", class_="ipc-link ipc-link--base ipc-link--inline")
         
         #source = requests.get(url) # "403 Client Error: Forbidden"
         #source.raise_for_status()
         #soup = BeautifulSoup(source.text, "html.parser")
         #storyline = soup.find("div", class_="ipc-html-content-inner-div")
+    """
+
+    #Extracting stuff from a movie's plot page
+    synopsisurls = []
+
+    for url in urls:
+        url = url + "plotsummary/?ref_=tt_stry_pl#synopsis"
+        synopsisurls.append(url)
+
+    for url in synopsisurls:
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        webpage = urlopen(req).read()
+
+        soup = BeautifulSoup(webpage, "html.parser")
+        
+        summaries = soup.find_all("div", class_="ipc-html-content-inner-div")
+        synopsis = str(summaries[10])
+
+        #remove tags
+        tags = re.findall(r"<[^<>]+>", synopsis)
+        for i in range(len(tags)):
+            synopsis = re.sub(r"<[^<>]+>", r"", synopsis)
+        
+        print(synopsis)
+        
     
 except Exception as e:
     print(e)
