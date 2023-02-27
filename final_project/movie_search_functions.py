056d52c16c34bc6aac35d54e044e44798f9fe372
gruksd <sofia.gruk@helsinki.fi>
2023-02-27 16:33:18 +0200

    Update movie_search_functions.py

--- final_project/movie_search_functions.py
+++ final_project/movie_search_functions.py
@@ -141,7 +141,7 @@ def search_t(synopsis_list, query): #tf-idf search
 
     except IndexError: # Entering an unknown word causes IndexError
         print("No matches")
-    best_doc_ids = sorted(best_doc_ids) 
+    
     return best_doc_ids
 
 def search_other():
